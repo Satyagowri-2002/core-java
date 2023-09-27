802f79c473caa96c3f3232c0d064f5a4bad630e9
acer <acer@TMP249-G2-M>
2023-09-27 14:52:41 +0530

    first commit

---
+++ project2/src/com/tns/inheritanceprog/SingleDemo.java
@@ -0,0 +1,10 @@
+package com.tns.inheritanceprog;
+
+public class SingleDemo {
+	public static void main(String[] args) {
+		Student st=new Student("satya",1234567,"penugonda",950206);
+		System.out.println(st);
+		
+	}
+
+}
--- project2/src/com/tns/inheritanceprog/SuperDemo.java
+++ project2/src/com/tns/inheritanceprog/SuperDemo.java
@@ -1,9 +1,13 @@
 package com.tns.inheritanceprog;
-
-public class SuperDemo {
+class parent{//base classar=100;
+	int var=100;
+}
+public class SuperDemo extends parent {//child class
 	int var=50;
 	void disply() {
-		System.out.println("the value"+var);
+		System.out.println("the child value is"+var);
+		System.out.println("the child value is"+super.var);
+
 	}
 	public static void main(String[] args) {
 		SuperDemo ob=new SuperDemo();
