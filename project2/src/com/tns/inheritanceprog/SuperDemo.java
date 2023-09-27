d8f6f5459454203b229aa552b29604afae5ad404
acer <acer@TMP249-G2-M.bbrouter>
2023-09-27 09:21:50 +0530

    first commit

---
+++ project2/src/com/tns/inheritanceprog/SuperDemo.java
@@ -0,0 +1,15 @@
+package com.tns.inheritanceprog;
+
+public class SuperDemo {
+	int var=50;
+	void disply() {
+		System.out.println("the value"+var);
+	}
+	public static void main(String[] args) {
+		SuperDemo ob=new SuperDemo();
+		ob.disply();
+	
+	
+	}
+
+}
