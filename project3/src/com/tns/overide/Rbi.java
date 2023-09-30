1f443b8f515cdff8b7babf144c0d3fae0158c36d
acer <acer@TMP249-G2-M>
2023-09-30 14:28:01 +0530

    first commit

---
+++ project3/src/com/tns/overide/Rbi.java
@@ -0,0 +1,10 @@
+package com.tns.overide;
+
+public class Rbi {
+
+	public float getRateofInterest() {
+		return 6.7f;
+	}
+}
+
+
---
+++ project3/src/com/tns/overide/Sbi.java
@@ -0,0 +1,10 @@
+package com.tns.overide;
+
+public class Sbi extends Rbi {
+	public float getRateofInterest() {
+		
+		System.out.println(super.getRateofInterest());
+		return 5.5f;
+	}
+
+}
