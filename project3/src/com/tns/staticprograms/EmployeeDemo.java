a14a01ba3dff4965ad07b69fbf70983fb71aa3bb
acer <acer@10.0.0.105>
2023-10-10 13:54:38 +0530

    firstommit c

---
+++ project3/src/com/tns/staticprograms/EmployeeDemo.java
@@ -0,0 +1,13 @@
+package com.tns.staticprograms;
+
+public class EmployeeDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Employee ob=new Employee("satya",777);
+System.out.println(ob);
+Employee companyName;//accessing the static variable using  class name
+
+	}
+
+}
